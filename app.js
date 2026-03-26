1c5c27c84d9719508794fe82dd9631c573d99110
Mauri784 <mauri784vega@gmail.com>
2026-03-26 16:54:09 -0600

    fix: replace var with const

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var client = require('prom-client');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const client = require('prom-client');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const itemsRouter = require('./routes/items');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var itemsRouter = require('./routes/items');
-
-var app = express();
+const app = express();
 
 // --- Prometheus ---
 client.collectDefaultMetrics();
