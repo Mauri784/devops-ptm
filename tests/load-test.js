20d62c18dec1d5de52cd352b28a14ae68c6c211b
Mauri784 <mauri784vega@gmail.com>
2026-03-26 17:32:19 -0600

    Update load-test.js

--- tests/load-test.js
+++ tests/load-test.js
@@ -11,7 +11,7 @@ export const options = {
 };
 
 export default function main() {
-  const res = http.get('http://localhost:3000/health');
+const res = http.get(`${__ENV.K6_HOST}/health`);
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
